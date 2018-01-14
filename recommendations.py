467af30205c86245ce87ed0ab4a8fe736a1a22f9
unknown <jantonz@gmail.com>
2018-01-14 13:34:25 +0100

    Updated recommendations

--- recommendations.py
+++ recommendations.py
@@ -5,7 +5,7 @@ Collective Intelligence
 Chapter 2: Making recommendations
 """
 # Nested dictionary of movie critics and their ratings of a small set of movies
-critics={"Lisa Rose": {"Lady in the Water": 2.5, "Snakes on a Plane": 3.5, "Just My Luck": 3.0, "Superman Returns": 3.5, "You, Me and Dupree": 2.5, "The Night Listener": 3.0}, "Gene Seymour": {"Lady in the Water": 2.5, "Snakes on a Plane": 3.5, "Just My Luck": 1.5, "Superman Returns": 5.0, "The Night Listener": 3.0, "You, Me and Dupree": 3.5}, "Michael Philipps": {"Lady in the Water": 2.5, "Snakes on a Plane": 3.0, "Superman Returns": 3.5, "The Night Listener": 4.0, "You, Me and Dupree": 2.5}, "Mick LaSalle": {"Lady in the Water": 3.0, "Snakes on a Plane": 3.5, "Just My Luck": 2.0, "Superman Returns": 3.0, "The Night Listener": 3.0, "You, Me and Dupree": 2.0}, "Jack Mathews": {"Lady in the Water": 3.0, "Snakes on a Plane": 4.0, "The Night Listener": 3.0, "Supermant Returns": 5.0, "You, Me and Dupree": 3.5}, "Toby": {"Snakes on a Plane": 4.5, "You, Me and Dupree": 1.0, "Superman Returns": 4.0}}
+critics={"Lisa Rose": {"Lady in the Water": 2.5, "Snakes on a Plane": 3.5, "Just My Luck": 3.0, "Superman Returns": 3.5, "You, Me and Dupree": 2.5, "The Night Listener": 3.0}, "Gene Seymour": {"Lady in the Water": 2.5, "Snakes on a Plane": 3.5, "Just My Luck": 1.5, "Superman Returns": 5.0, "The Night Listener": 3.0, "You, Me and Dupree": 3.5}, "Michael Philipps": {"Lady in the Water": 2.5, "Snakes on a Plane": 3.0, "Superman Returns": 3.5, "The Night Listener": 4.0, "You, Me and Dupree": 2.5}, "Mick LaSalle": {"Lady in the Water": 3.0, "Snakes on a Plane": 3.5, "Just My Luck": 2.0, "Superman Returns": 3.0, "The Night Listener": 3.0, "You, Me and Dupree": 2.0}, "Jack Mathews": {"Lady in the Water": 3.0, "Snakes on a Plane": 4.0, "The Night Listener": 3.0, "Superman Returns": 5.0, "You, Me and Dupree": 3.5}, "Toby": {"Snakes on a Plane": 4.5, "You, Me and Dupree": 1.0, "Superman Returns": 4.0}}
 
 
 ## Euclidean distance score
@@ -31,6 +31,10 @@ def sim_distance(prefs, person1, person2):
                         for item in prefs[person1] if item in prefs[person2]])
     return 1 / (1 + sqrt(sum_of_squares))
     # The list of shared items "si" has not been used so far
+    
+import matplotlib as plt
+
+
     
 ## Pearson correlation score
 # This returns the Pearson correlation coefficient between p1 and p2
@@ -72,3 +76,52 @@ def sim_pearson(prefs, p1, p2):
     return r
 
 ## Ranking the critics
+    
+# Returns the best matches for person from the prefs dictionary
+# Number of results and similarity are optional parameters
+    
+def topMatches(prefs, person, n = 5, similarity = sim_pearson):
+    scores = [(similarity(prefs, person, other), other) for other in prefs if other != person]
+    
+    #Sort the list so the highest scores appear at the top
+    scores.sort()
+    scores.reverse()
+    return scores[0:n]
+
+## Recommending items
+    
+# Gets recommendations for a person by using a weighting average
+# of every other user's rankings
+    
+# This code loops through every other person in the prefs dictionary. In each case, it calculates how similar they are to th person specified. It then loops through every item for which they've given a score. The line in bold shows how the final score for an item is calculated (the score for each item si multiplied by the similarity and these products are added together). The scores are normalized by dividing by the similarity sum and the sorted results are returned.
+
+def getRecommendations(prefs, person, similarity = sim_pearson):
+    totals = {}
+    simSums = {}
+    for other in prefs:
+        # Don't compare it to myself
+        if other == person: continue
+        sim = similarity(prefs, person, other)
+        
+        # Ignore scores of zero or lower
+        if sim <= 0: continue
+        for item in prefs[other]:
+            
+            # Only scores movies I haven't seen yet
+            if item not in prefs[person] or prefs[person][item] == 0:
+                
+                # Similarity * Score
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item]*sim
+                
+                # Sum of similarities
+                simSums.setdefault(item, 0)
+                simSums[item] += sim # simSums = simSums + sim
+                
+    # Create the normalized list
+    rankings = [(total / simSums[item], item) for item,total in totals.items()]
+    
+    # Return the sorted list
+    rankings.sort()
+    rankings.reverse()
+    return rankings
